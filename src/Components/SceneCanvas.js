55702c8e28f3bbd5cbe5daaf43d0bb2bbf837e74
Garrett Burns <garrettburns@mac.com>
2020-06-25 15:28:24 -0700

    Add files via upload

---
+++ src/Components/SceneCanvas.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import CheckClickCollision from './CheckClickCollision.js';
+
+
+class SceneCanvas extends React.Component{
+
+    componentDidMount = () => {
+        //Renders the canvas with game objects
+        this.prepareCanvasObjects();
+    }
+
+    prepareCanvasObjects = () => {
+        //The reference to the canvas and other info tied to variables
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext("2d");
+        const rect = canvas.getBoundingClientRect();
+        //Sets a BG color
+        this.setBackground(canvas, "#4B4B55");
+
+        //This is the event listener for the clicks that check for collision with game elements
+        canvas.addEventListener('click', e => {
+            let object = {};
+            //Finds the difference from the (0, 0) of the canvas
+            object.x = (e.clientX - rect.left);
+            object.y =  (e.clientY - rect.top);
+            //Calculates whether the click touched an object
+            let collision = CheckClickCollision(this.props.levelObjects, object.x, object.y);
+
+            if(collision){
+                this.checkContext(collision[0], collision[1]);
+            }
+        })
+
+        this.renderCanvasObjects(ctx)
+        this.renderDialog(ctx, this.props.worldState.dialogBox[this.props.worldState.dialogCurrent], [30, 50]);
+        
+    }
+
+    renderCanvasObjects = (ctx) => {
+        this.props.levelObjects.map(obj => {
+
+            //Seperates the data into individual numbers
+            let pos = obj.position.split(' ');
+            let dimensions = obj.size.split(' ');
+
+            //Sets the rectangle color
+            if(obj.color){
+                ctx.fillStyle = obj.color;
+            }else{
+                ctx.fillStyle = "#FFFF33";
+            }
+            
+            //Creates a rectangle with the fillStyle
+            ctx.fillRect(pos[0],pos[1],dimensions[0],dimensions[1]);
+        })
+    }
+
+    renderDialog = (ctx, text, position, fontSize="30px", font="Arial") => {
+        let worldState = this.props.worldState;
+        console.log(this.props.worldState);
+
+        if(worldState.dialogBox[worldState.dialogCurrent]){
+                text = worldState.dialogBox[worldState.dialogCurrent]
+                ctx.font = fontSize + " " + font;
+                ctx.fillStyle = "#000000";
+                ctx.fillText(text, position[0], position[1]);
+        }
+
+        
+    }
+
+    setBackground = (canvas, backgroundColor) => {
+        canvas.style.background = backgroundColor;
+    }
+
+    checkContext = (index, object) => {
+        switch(object.context){
+            case "talk": this.props.onClick(index, 'talk')
+            break;
+            case "pickup": this.props.onClick(index, 'pickup')
+            break;
+            case "dialogue": this.props.onClick(index, 'dialogue')
+            break; 
+            default: this.props.onClick(index, "impossible")
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <canvas ref="canvas" width={750} height={450} />
+            </div>
+        )
+    }
+    
+}
+/*
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
+ctx.fillText("Hello World", 10, 50);
+
+                if( (clickX > parseInt(coords[1])) && (clickX < (parseInt(coords[1]) + noteCollection[note].width)) ){
+                    if(noteCollection[note].status == true){
+                        this.props.noteHit(note, songId)
+                    }
+                }
+
+                if( (clickX > parseInt(coords[1])) && (clickX < (parseInt(coords[1]) + noteCollection[note].width)) && (clickY > parseInt(coords[0])) && (clickY < (parseInt(coords[0]) + noteCollection[note].height)) ){
+                    if(noteCollection[note].status == false){
+                        this.noteClicked(note, songId)
+                    }else if(noteCollection[note].status == true){
+                        this.noteClicked(note, songId)
+                    }
+                }
+
+
+const gameCanvas = (props) => {
+    return (<canvas width = "1200" height = "800" ></canvas>);
+}
+*/
+
+
+export default SceneCanvas;
---
+++ src/MainPage.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import SceneCanvas from './Components/SceneCanvas';
+import AreaCanvas from './Components/AreaCanvas.js';
+import Dialog from './Containers/Dialog.js'
+
+
+class MainPage extends React.Component{
+    //6093E1 loading
+    //4F7AAA general
+
+    //Could a container component replace most of state?
+    state = {
+        everythingLoaded: true,
+        levels: {
+            seattle:{
+                //This is generally what I'm imagining the level-object structure is going to be like
+                //This information should be enough to render an image on the canvas with a spot to look out for
+                pikePlaceMarket:[
+                    {name: "Clark", position: "300 122", size: "90 200", sprite: "?", context: "talk", color: "#33AAFF"},
+                    {name: "Dlark", position: "400 122", size: "90 200", sprite: "?", context: "talk", color: "#33AAFF"}
+                ]
+            }
+        },
+        worldState: {
+            //I'm using the state here to store the player's location
+            currentCity: "seattle",
+            currentSpot: "pikePlaceMarket",
+            //this sets the dialog appearing onscreen
+            talkingTo: null,
+            dialogBox: [],
+            dialogCurrent: -1,
+            //These aren't permanent or set in stone but for testing interactions and how they are recorded
+            peopleTalkedTo: 0,
+            informationCollected: 0,
+            testObjectIntereacted: false
+        }
+    }
+
+    handleClick = (index, context) => {
+        console.log(context)
+        //readability variables
+        let worldState = this.state.worldState;
+        let levels = this.state.levels;
+        //Finds the object in the state by the given index
+        let object = levels[worldState.currentCity][worldState.currentSpot][index];
+        if(context == "talk"){
+            //runs the npcTalk script
+            this.npcTalk(object);
+        }
+    }
+
+    npcTalk = (object, specs=null) => {
+
+        console.log(object)
+        //prepares shortened variable for readability
+        let oldState = this.state;
+        let currentSpot = oldState.levels [oldState.worldState.currentCity] [oldState.worldState.currentSpot];
+        //let dialogBox = oldState.worldState.dialogBox;
+        //unrender and wait until state is updated
+        oldState.everythingLoaded = false;
+        this.setState(oldState);
+
+        //checks if there's a text and if there isn't, make one in the object list
+        if(!currentSpot.find(obj => obj.name == "textBox")){
+            currentSpot.push({name: "textBox", position: "20 20", size: "600 90", sprite: "?", color: "#FFFFFF"});
+        }
+
+        //Finds the dialog array in the Dialog object file and sets it as the dialogBox
+        oldState.worldState.dialogBox = Dialog(object.name, specs);
+        //If there's a dialog line at the next index to render...
+        if(oldState.worldState.dialogBox[oldState.worldState.dialogCurrent + 1]){
+            //add 1 to the current dialog index
+            oldState.worldState.dialogCurrent += 1;
+
+        //if there isn't any lines next...
+        }else{
+            //reset the values to go back
+            oldState.worldState.dialogCurrent = -1;
+            oldState.worldState.dialogBox = [];
+            let textBoxIndex = currentSpot.findIndex(obj => obj.name == "textBox", 1);
+            currentSpot.splice(textBoxIndex, 1);
+        }
+        
+        //gives the okay for the element to re-render
+        oldState.everythingLoaded = true;
+        this.setState(oldState);
+        //console.log(this.state);
+    }
+
+    renderScene(){
+        //This uses the current city and spot within the city to navigate the levels object and find the objects to put onscreen
+        let levelObjects = this.state.levels [this.state.worldState.currentCity] [this.state.worldState.currentSpot];
+
+        return(
+            <div>
+                < SceneCanvas key={SceneCanvas} onClick={this.handleClick} levelObjects={levelObjects} worldState={this.state.worldState} />
+            </div>
+        );
+    }
+
+    renderArea(people){
+        //This uses the current city and spot within the city to navigate the levels object and find the objects to put onscreen
+        let levelObjects = this.state.levels [this.state.worldState.currentCity] [this.state.worldState.currentSpot];
+
+        return(
+            <div>
+                < AreaCanvas key={AreaCanvas} onClick={this.handleClick} levelObjects={levelObjects} worldState={this.state.worldState} />
+            </div>
+        );
+    }
+    
+    render(){
+            return (
+                <Router>
+                    <div>
+                        <div>
+                            <Link to="/">Main</Link> <Link to="/Scene">Play</Link>
+                        </div>
+                        <Switch>
+                            <Route path='/' exact>
+                                {this.state.everythingLoaded ? this.renderScene() : null}
+                            </Route>
+                            <Route path='/Scene' exact>
+                                {this.state.everythingLoaded ? this.renderScene() : null}
+                            </Route>
+                            <Route path='/Area' exact>
+                                {this.state.everythingLoaded ? this.renderArea() : null}
+                            </Route>
+                        </Switch>
+                        
+                    </div>
+                </Router>
+               
+            )
+    }
+
+}
+
+export default MainPage;
